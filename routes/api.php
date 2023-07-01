bcbffc8418b2538372aa554d27c1e825b6cb0a88
muhohoweb <muhohokenya@users.noreply.github.com>
2023-07-01 10:31:05 +0300

    Deploy via github actions

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,9 @@ use Illuminate\Support\Facades\Route;
 Route::post("register", [AuthController::class,'register']);
 Route::post("login", [AuthController::class,'login']);
 
-
+Route::get('hello',function (){
+    return "Hello deployment";
+});
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get("user", [AuthController::class,'getUser']);
 
