8be468df93ed5a3d02bf3f6add137c34fae7e0d0
muhohoweb <muhohokenya@users.noreply.github.com>
2023-07-01 13:40:52 +0300

    Remove hello test api

--- routes/api.php
+++ routes/api.php
@@ -22,10 +22,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post("register", [AuthController::class,'register']);
 Route::post("login", [AuthController::class,'login']);
-
-Route::get('hello',function (){
-    return "Hello deployment";
-});
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get("user", [AuthController::class,'getUser']);
 
