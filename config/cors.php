2af438003bcd271903b7a39848b374b6f2ccb943
muhohoweb <muhohokenya@users.noreply.github.com>
2023-07-07 00:22:19 +0300

    Enable All origins

--- config/cors.php
+++ config/cors.php
@@ -22,7 +22,7 @@ return [
     //
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => ['http://localhost'],
+    'allowed_origins_patterns' => ['*'],
 
     'allowed_headers' => ['*'],
 
