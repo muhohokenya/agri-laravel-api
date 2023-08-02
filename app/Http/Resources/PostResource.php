cd7e09d940ecff93273b0026fae5f1ad87057cc5
muhohoweb <muhohokenya@users.noreply.github.com>
2023-08-02 15:25:39 +0300

    Load Post with count

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -17,6 +17,14 @@ class PostResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            "id"=>$this->id,
+            "image"=>$this->image,
+            "title"=>$this->title,
+            "description"=>$this->description,
+            "user"=>$this->user,
+            "replies"=>$this->replies,
+            "votes"=>$this->up_votes,
+        ];
     }
 }
